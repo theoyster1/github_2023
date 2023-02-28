f72efc167809223e0033f0c49b45f378d0757739
theoyster1 <125879588+theoyster1@users.noreply.github.com>
2023-02-28 12:03:14 +0100

    Delete capp directory

--- capp/home/routes.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template, Blueprint
-
-home = Blueprint('home',__name__)
-
-@home.route('/')
-@home.route('/home')
-def Home():
-  return render_template('Home.html', title="Home")
--- capp/methodology/routes.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template, Blueprint
-
-methodology = Blueprint('methodology',__name__)
-
-
-@methodology.route('/methodology')
-def Methodology():
-  return render_template('Methodology.html', title='Methodology')

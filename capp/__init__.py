31735ce9e4efe3f14c892870b47dc3286728f3be
theoyster1 <125879588+theoyster1@users.noreply.github.com>
2023-03-06 10:09:32 +0100

    Add files via upload

--- capp/__init__.py
+++ capp/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+import os
 
 application = Flask(__name__)
+
+application.config['SECRET_KEY'] = '3oueqkfdfas8ruewqndr8ewrewrouewrere44554'
 
 from capp.home.routes import home
 from capp.methodology.routes import methodology
